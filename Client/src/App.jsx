a70c9284018799fe7a8acfa434f51e849a53361c
Joel Gt <0332201030@unjfsc.edu.pe>
2024-11-18 23:58:48 -0500

    Integracion de las rutas con reacto dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,23 @@
-import { Button } from "./components/ui/button"
-
+import { Button } from "@/components/ui/button"
+import { BrowserRouter,Navigate,Route, Routes } from "react-router-dom";
+import { Auth } from "./pages/auth";
+import { Chat } from "./pages/chat";
+import { Profile } from "./pages/profile";
 
 export const App = () => {
   return (
-    <div className="container bg-black  h-screen">
-      <Button>App</Button>
-    </div>
+   <>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={ <Auth/> }/>
+        <Route path="*" element={ <Navigate to="/auth"/> } />
+        <Route path="/chat" element={ <Chat/> }/>
+        <Route path="/profile" element={ <Profile/> }/>
+      </Routes>
+
+    </BrowserRouter>   
+   </>
     
   )
 }
