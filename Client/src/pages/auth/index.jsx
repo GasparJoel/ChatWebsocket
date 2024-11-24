c0e4751cf7068404095fd9320e053476eeee9993
Joel Gt <0332201030@unjfsc.edu.pe>
2024-11-23 23:54:28 -0500

    Integre codigo tailwind para el auth

--- Client/src/pages/auth/index.jsx
+++ Client/src/pages/auth/index.jsx
@@ -1,7 +1,19 @@
-
-
 export const Auth = () => {
   return (
-    <div>Auth</div>
-  )
-}
+    <div className="h-[100vh] w-[100vh] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white 
+      text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] 
+       rounded-3xl grid xl:grid-cols-2 ">
+        <div className="flex flex-col gap-10 justify-center items-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl ">Welcome</h1>
+            </div>
+          </div>
+          auth
+        </div>
+        
+      </div>
+    </div>
+  );
+};
