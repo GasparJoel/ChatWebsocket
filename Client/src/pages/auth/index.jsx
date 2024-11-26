a24050e6e6b5ffb5908d9a465b3bf1cea4f1415d
Joel Gt <0332201030@unjfsc.edu.pe>
2024-11-26 00:00:53 -0500

    Integre codigo tailwind para armar los tabs de shadcn para el login y signup

--- Client/src/pages/auth/index.jsx
+++ Client/src/pages/auth/index.jsx
@@ -1,18 +1,58 @@
+import Victory from "@/assets/victory.svg";
+import Background from "@/assets/login2.png";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { Input } from "@/components/ui/input";
+
 export const Auth = () => {
   return (
     <div className="h-[100vh] w-[100vh] flex items-center justify-center">
-      <div className="h-[80vh] bg-white border-2 border-white 
+      <div
+        className="h-[80vh] bg-white border-2 border-white 
       text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] 
-       rounded-3xl grid xl:grid-cols-2 ">
+       rounded-3xl grid xl:grid-cols-2 "
+      >
         <div className="flex flex-col gap-10 justify-center items-center">
           <div className="flex items-center justify-center flex-col">
             <div className="flex items-center justify-center">
-              <h1 className="text-5xl font-bold md:text-6xl ">Welcome</h1>
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+              <img src={Victory} alt="Victory emoji" className="h-[100px]" />
             </div>
+            <p className="font-medium text-center">
+              Fill in the details to get started with the best chat app!
+            </p>
           </div>
-          auth
+          <div className="flex items-center justify-center w-full">
+            <Tabs className="w-3/4">
+              <TabsList className="flex justify-center border-b-2 bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90
+                rounded-none w-full text-center data-[state=active]:text-black 
+                data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 
+                transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90
+                rounded-none w-full text-center data-[state=active]:text-black 
+                data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 
+                transition-all duration-300"
+                >
+                  Signup
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login">
+                 <Input type="email" placeholder="Email" />
+                <p>Login form goes here</p>
+              </TabsContent>
+              <TabsContent value="signup">
+                <p>Signup form goes here</p>
+              </TabsContent>
+            </Tabs>
+          </div>
         </div>
-        
       </div>
     </div>
   );
