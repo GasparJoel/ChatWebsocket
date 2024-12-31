91507c05f0385f538541ecfe8059983633e32b37
Joel Gt <0332201030@unjfsc.edu.pe>
2024-12-30 23:12:07 -0500

    Desarrollando el midleware Signup con token, cookie

---
+++ Server/controllers/AuthController.js
@@ -0,0 +1,51 @@
+import User from "../models/UserModel";
+import  jwt  from "jsonwebtoken";
+
+const maxAge = 3*24*60*60*60 ;
+
+const createToken=(email,userId)=>{
+    return  jwt.sign({email,userId},process.env.JWT_KEY,{expiresIn:maxAge})
+}
+
+export const signup= async (req,res,next)=>{
+    try {
+
+          
+        const {email,password} = req.body;
+         // Validar que email y password estén presentes
+         if (!email || !password ) {
+            return res.status(400).send('Email and password is required')
+         }
+
+          // Verificar si el usuario ya existe
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+            }
+
+          // Crear el usuario en la base de datos
+         const usernew = await User.create({email,password});
+
+         const token  = createToken(email,usernew.id)
+
+         // Establecer la cookie del token
+        res.cookie("jwt",token,{
+            maxAge:maxAge *1000,// Convertir segundos a milisegundos
+            httpOnly:true, // Asegurar que la cookie no sea accesible desde el cliente
+            secure:true,
+            sameSite:"None" // Permitir cookies en distintos dominios
+        })
+
+        return res.status(201).json({
+            user:{
+                id:usernew.id,
+                email:usernew.email,
+            }
+        })
+
+         
+    } catch (error) {
+     console.log(error)   
+     return res.status(500).send('Internal Server Error')
+    }
+}
--- Server/models/UserModel.js
+++ Server/models/UserModel.js
@@ -1,4 +1,5 @@
 import  mongoose from "mongoose";
+import {genSalt, hash} from "bcrypt";
 
 const userSchema = new  mongoose.Schema({
     email:{
@@ -36,3 +37,14 @@ const userSchema = new  mongoose.Schema({
         default:false,
     }
 })
+
+//Middleware que se ejecuta antes de la accion del save 
+userSchema.pre("save",async function (next) {
+   // Vamos a incriptar la contraseña
+   const salt = await genSalt(); // Genera un "salt" (valor aleatorio para hacer más segura la encriptación)
+   this.password = await hash(this.password, salt); // Encripta la contraseña usando el salt
+   next(); // Llama a `next` para continuar con el flujo de guardado
+})
+
+const User=mongoose.model("Users",userSchema);
+export default User;
